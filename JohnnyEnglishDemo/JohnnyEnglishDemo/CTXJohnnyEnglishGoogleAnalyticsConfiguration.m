2f989e489c7e0f5feb74d5f0f99991262172ae94
Dmitry Makarenko <dmitry.a.makarenko@gmail.com>
2014-10-06 16:05:29 +0100

    Minor: some additional information was added regarding to how to ignore further changes in the version-controlled files.

--- JohnnyEnglishDemo/JohnnyEnglishDemo/CTXJohnnyEnglishGoogleAnalyticsConfiguration.m
+++ JohnnyEnglishDemo/JohnnyEnglishDemo/CTXJohnnyEnglishGoogleAnalyticsConfiguration.m
@@ -7,8 +7,11 @@
 //  Licensed under the MIT license.
 //
 
-
 #warning Please use your Tracking ID below.
 
 // Please don't commit your tracking ID in project repository as it's a public repo.
+
+// You can use a git trick described here to hide your modifications from being committed:
+// http://stackoverflow.com/a/4633776/439529
+
 NSString * const CTXJohnnyEnglishGoogleAnalyticsConfigurationTrackingID = @"Please put your Tracking ID here";
