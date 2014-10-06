d7d65b5afa4e7ad74c4d1c56d72585314a7e5264
Dmitry Makarenko <dmitry.a.makarenko@gmail.com>
2014-10-06 15:36:45 +0100

    [GOOGLE ANALYTICS] An empty header file which will contain Google Analytics configuration information was added to the project.

---
+++ JohnnyEnglishDemo/JohnnyEnglishDemo/CTXJohnnyEnglishGoogleAnalyticsConfiguration.h
@@ -0,0 +1,10 @@
+//
+//  CTXJohnnyEnglishGoogleAnalyticsConfiguration.h
+//  JohnnyEnglish
+//
+//  Created by Dmitry Makarenko on 06/10/2014.
+//  Copyright (c) 2014 EF CTX. All rights reserved.
+//  Licensed under the MIT license.
+//
+
+extern NSString * const CTXJohnnyEnglishGoogleAnalyticsConfigurationTrackingID;
---
+++ JohnnyEnglishDemo/JohnnyEnglishDemo/CTXJohnnyEnglishGoogleAnalyticsConfiguration.m
@@ -0,0 +1,14 @@
+//
+//  CTXJohnnyEnglishGoogleAnalyticsConfiguration.h
+//  JohnnyEnglish
+//
+//  Created by Dmitry Makarenko on 06/10/2014.
+//  Copyright (c) 2014 EF CTX. All rights reserved.
+//  Licensed under the MIT license.
+//
+
+
+#warning Please use your Tracking ID below.
+
+// Please don't commit your tracking ID in project repository as it's a public repo.
+NSString * const CTXJohnnyEnglishGoogleAnalyticsConfigurationTrackingID = @"Please put your Tracking ID here";
