8f63a4797d50c4d692d0333f6ae62f14b08c062c
Stefan Ceriu <stefan.ceriu@yahoo.com>
2018-11-27 16:07:29 +0200

    Add custom hashing to custom definition keys.

--- Sources/CTXCustomDefinitionKey.m
+++ Sources/CTXCustomDefinitionKey.m
@@ -8,6 +8,13 @@
 
 #import "CTXCustomDefinitionKey.h"
 
+#ifndef CTX_NSUINTEGER_BIT
+#define CTX_NSUINTEGER_BIT (CHAR_BIT * sizeof(NSUInteger))
+#endif
+#ifndef CTX_NSUINTROTATE
+#define CTX_NSUINTROTATE(val, howmuch) ((((NSUInteger)(val)) << (howmuch)) | (((NSUInteger)(val)) >> (CTX_NSUINTEGER_BIT - (howmuch))))
+#endif
+
 @interface CTXCustomDefinitionKey ()
 
 @property (nonatomic, strong) NSString *name;
@@ -37,6 +44,21 @@
     [copy setName:self.name.copy];
     [copy setKey:self.key.copy];
     return copy;
+}
+
+- (NSUInteger)hash
+{
+    NSUInteger hashResult = 31;
+    
+    int denominator = 1;
+    
+    ++denominator;
+    hashResult = CTX_NSUINTROTATE([self.name hash], CTX_NSUINTEGER_BIT / denominator) ^ hashResult;
+    
+    ++denominator;
+    hashResult = CTX_NSUINTROTATE([self.key hash], CTX_NSUINTEGER_BIT / denominator) ^ hashResult;
+    
+    return hashResult;
 }
 
 @end
